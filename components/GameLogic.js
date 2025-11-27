3e1844657718ecc5a5de61a4cc31cd34771fed40
Alejandro Castaño <alejandro.castano@securitasdirect.es>
2025-11-27 15:12:54 +0100

    Change: Oberon ahora ve a Asesino y Morgana (pero nadie lo ve a él)

--- components/GameLogic.js
+++ components/GameLogic.js
@@ -84,8 +84,8 @@ export const ROLES = {
     name: 'Oberon',
     emoji: '🕴️',
     team: 'evil',
-    description: 'Espía solitario - invisible para todos, no ve a nadie',
-    canSee: [], // No ve a ningún malo
+    description: 'Espía solitario - invisible para todos. Ve solo a Asesino y Morgana',
+    canSee: ['ASESINO', 'MORGANA'], // Ve solo a los dos roles principales malos
     specialAbility: 'Invisible para todos los demás roles',
     winCondition: 'Los malos sabotean 3 misiones O asesinan a Merlín'
   }
